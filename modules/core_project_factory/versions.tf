1e986a1a37699625bfcdb2f77a6b0eae1194c8d9
Neil Stewart <neil.stewart@cts.co>
2021-05-27 11:09:07 +0100

    increasing support for tf 15 and upgrading null/random module

--- modules/core_project_factory/versions.tf
+++ modules/core_project_factory/versions.tf
@@ -15,12 +15,12 @@
  */
 
 terraform {
-  required_version = ">=0.12.6, <0.14"
+  required_version = ">=0.12.6, <0.16"
 
   required_providers {
     google      = ">= 3.8, < 4.0"
     google-beta = ">= 3.8, < 4.0"
-    null        = "~> 2.1"
-    random      = "~> 2.2"
+    null        = "~> 3.1.0"
+    random      = ">= 3.1.0"
   }
 }
